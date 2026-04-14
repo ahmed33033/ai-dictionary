5efaa0e941339015313f48092b9cdffde02943d1
Ahmed Mohamed <ahmed.akod@outlook.com>
2026-04-14 21:38:03 +0000

    Added a custom search in progress

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,32 @@
+"use client";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import { Field } from "@/components/ui/field";
 import { Input } from "@/components/ui/input";
+import { useState } from "react";
+import {
+  Command,
+  CommandDialog,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+  CommandSeparator,
+  CommandShortcut,
+} from "@/components/ui/command";
+import * as defJSONS from "@/app/terms.json";
 
 export default function Home() {
+  const [searchVal, setSearchVal] = useState(false);
+  const defJSON = defJSONS.definitions;
   return (
     <>
       <header>
-        <div className="md:flex text-center md:text-left items-center bg-indigo-200 py-5 px-10 rounded-lg">
+        <div className="md:flex text-center md:text-left items-center bg-indigo-200 py-5 px-10 rounded-lg border-main-color border-3">
           <div className="inline-block max-md:mbe-3 md:grow-1">
             <div className="inline-block text-center">
-              <Link className="text-xl font-sans text-indigo-800 inline-block w-m" href={"/"}>
+              <Link className="text-xl font-sans text-main-color inline-block w-m" href={"/"}>
                 The Simple <br />
                 <span className="text-4xl font-bold">AI Dictionary</span>
               </Link>
@@ -21,7 +37,7 @@ export default function Home() {
             </div>
           </div>
           <nav className="inline">
-            <ol className="*:inline *:not-last:mr-10 text-lg font-sans underline *:hover:bg-yellow-200 *:p-2 *:rounded-md">
+            <ol className="*:inline *:not-last:mr-10 text-main-color text-lg font-sans underline *:hover:bg-yellow-200 *:p-2 *:rounded-md">
               <li>
                 <Link href={"/about"}>About</Link>
               </li>
@@ -31,12 +47,24 @@ export default function Home() {
             </ol>
           </nav>
         </div>
-        <Field orientation="horizontal" className="my-5 *:h-20 *:text-lg! ">
-          <Input type="search" placeholder="Search the buzzwords..." />
-          <Button className="w-30"> Search</Button>
-        </Field>
+        <search className="my-5">
+          <label
+            className="block text-center text-4xl font-black bg-linear-to-r from-blue-500 to-pink-500 bg-clip-text text-transparent mb-2"
+            htmlFor="search-dict"
+          >
+            Name the Buzzword...
+          </label>
+          <div className="flex justify-center">
+            <input
+              className="border-main-color border-3 grow-1 placeholder:italic indent-4 rounded-s-lg"
+              id="search-dict"
+              placeholder="Search..."
+              type="search"
+            />
+            <button className="text-white bg-main-color p-3 px-7 rounded-e-lg">Search</button>
+          </div>
+        </search>
       </header>
-
       <main>
         <article>
           <h1>Word of the Day</h1>
