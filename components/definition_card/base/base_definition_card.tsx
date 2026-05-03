eab87c0969452b4178e3ad6bd9b6c9e9fa454ed3
Ahmed <ahmed.akod@outlook.com>
2026-05-03 16:20:38 -0400

    styling card with container queries

--- components/definition_card/base/base_definition_card.tsx
+++ components/definition_card/base/base_definition_card.tsx
@@ -4,9 +4,11 @@ import { DiagramImage } from "./diagram_image";
 
 export function BaseDefinitionCard({ children, diagram }: { children: Readonly<React.ReactNode>; diagram?: diagram }) {
   return (
-    <Card className="@container max-w-2xl gap-2 flex flex-row pr-5">
-      <div>{children}</div>
-      {diagram !== undefined && <DiagramImage diagram={diagram} />}
-    </Card>
+    <div className="@container">
+      <Card className="max-w-2xl gap-2 flex flex-col pr-5 @xl:flex-row items-center">
+        <div>{children}</div>
+        {diagram !== undefined && <DiagramImage diagram={diagram} />}
+      </Card>
+    </div>
   );
 }
