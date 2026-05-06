31cdef031684d3a14d24560234cc68dca82b0e1d
Ahmed <ahmed.akod@outlook.com>
2026-05-06 11:39:52 -0400

    remove mdx support from app

--- next.config.ts
+++ next.config.ts
@@ -1,21 +1,13 @@
 import type { NextConfig } from "next";
-import createMDX from "@next/mdx";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  // Configure `pageExtensions` to include markdown and MDX files
   output: "export",
-  pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
+  pageExtensions: ["js", "jsx", "ts", "tsx"],
   basePath: "/ai-dictionary",
   images: {
     unoptimized: true,
   },
 };
 
-const withMDX = createMDX({
-  // Add markdown plugins here, as desired
-  extension: /\.(md|mdx)$/,
-});
-
-// Merge MDX config with Next.js config
-export default withMDX(nextConfig);
+export default nextConfig;
