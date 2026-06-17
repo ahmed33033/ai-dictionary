da4df179bb137f62d4db7928b1a958bffe4a1e38
Ahmed <ahmed.akod@outlook.com>
2026-06-17 17:26:58 -0400

    add basic contribution text

--- app/contribute/page.tsx
+++ app/contribute/page.tsx
@@ -1,5 +1,6 @@
 import { ProseH1 } from "@/components/prose/ProseHeaders";
 import { ProseP } from "@/components/prose/ProseP";
+import Link from "next/link";
 
 export default function Contribute() {
   return (
@@ -8,7 +9,7 @@ export default function Contribute() {
       <ProseP className="text-center">
         As an opensource project, the Simple AI Dictionary is always looking for contributors!
       </ProseP>
-      <ProseP className="text-center italic">(More information to be added soon...)</ProseP>
+      <ProseP className="text-center ">Whether that's contributing a new definition, critiquing an existing one or even suggesting some stylistic changes, all valued contributions are welcome through the <Link href="https://github.com/ahmed33033/ai-dictionary" className="underline decoration-2">Github Repository</Link></ProseP>
     </div>
   );
 }
